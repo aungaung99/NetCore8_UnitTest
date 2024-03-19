0678baaf60e510beb0eacf6cfe2092c607cc9a39
Aung Naing Oo <aungaung20.ab@gmail.com>
2024-03-19 23:39:41 +0630

    add api unit test

---
+++ NetCore8_UnitTest.APIs.Tests/Controllers/AggregateControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+
+using Microsoft.AspNetCore.Mvc;
+
+using NetCore8_UnitTest.APIs.Controllers;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCore8_UnitTest.APIs.Tests.Controllers
+{
+	public class AggregateControllerTest
+	{
+		private AggregateController _sut;
+		public AggregateControllerTest()
+		{
+			_sut = new AggregateController();
+		}
+
+		[Theory]
+		[InlineData(1, 2)]
+		public void Sum_ShouldBeGreaterThanZero_WhereNatural(int a, int b)
+		{
+			var result = _sut.Sum(a, b);
+			Assert.Equal(3, result);
+			result.Should().NotBeNull();
+			result.Should().BeAssignableTo<ActionResult<int>>();
+			result.Result.Should().BeAssignableTo<OkObjectResult>();
+		}
+	}
+}
---
+++ NetCore8_UnitTest.APIs/Controllers/AggregateController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore8_UnitTest.APIs.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AggregateController : ControllerBase
+	{
+		[HttpGet]
+		public ActionResult<int> Sum(int a, int b)
+		{
+			return Ok(a + b);
+		}
+	}
+}
